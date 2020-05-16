dc7aa9f3ee821f1c5fdad9569c4972739ee3a8ea
Yiwen Fang <fang.yiw@husky.neu.edu>
2020-05-16 03:08:44 -0700

    Add older browsers support

--- pro-mern-stack-2/server/server.js
+++ pro-mern-stack-2/server/server.js
@@ -5,5 +5,5 @@ const app = express();
 app.use(express.static('public'));
 
 app.listen(3000, function () {
-    console.log('App started on port 3000');
+  console.log('App started on port 3000');
 });
