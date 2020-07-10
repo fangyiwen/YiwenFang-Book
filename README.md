73abe66766bc72ca07730a30092d9677964fd50c
Yiwen Fang <fang.yiw@husky.neu.edu>
2020-07-10 01:02:47 -0700

    Create basic server rendering

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ This is Dr. Yiwen Fang's repository for the project described in the book Pro ME
 ## Chapter 12
 
 * Create new directory structure by splitting the current source code into three directories. Note: 1) Delete right curly bracket in Page 378; 2) "rules", "error", "always", and "ignorePackages" should be in double quotes.
+* Create basic server rendering by creating an HTML on the server side via `ReactDOMServer.renderToString()`.
 
 
 ---
--- pro-mern-stack-2/ui/server/uiserver.js
+++ pro-mern-stack-2/ui/server/uiserver.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 
+const render = require('./render.js');
+
 const app = express();
 
 const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
@@ -41,6 +43,8 @@ app.get('/env.js', (req, res) => {
   res.set('Content-Type', 'application/javascript');
   res.send(`window.ENV = ${JSON.stringify(env)}`);
 });
+
+app.get('/about', render);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('public/index.html'));
